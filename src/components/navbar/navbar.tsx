9b6c59995ab8b323ad9cec64df715d4016582fb9
jvalverde96 <javier96.vs@gmail.com>
2025-01-15 00:20:27 -0600

    feat: add redirect links on Navbar component

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,11 +1,16 @@
 import { ShoppingCart } from 'lucide-react';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <header className="bg-header">
       <nav className="h-[64px] flex items-center justify-between mx-auto px-6 py-5 lg:px-32 lg:py-5 lg:max-w-[1536px]">
-        <span className="text-2xl leading-6 tracking-[0.4px]">Gamer Shop</span>
-        <ShoppingCart />
+        <Link href="/" className="text-2xl leading-6 tracking-[0.4px]">
+          Gamer Shop
+        </Link>
+        <Link href="/cart">
+          <ShoppingCart />
+        </Link>
       </nav>
     </header>
   );
