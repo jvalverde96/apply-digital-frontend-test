9ad2be68cd093e9b7a78a6da85eedd5a57a23915
jvalverde96 <javier96.vs@gmail.com>
2025-01-19 10:40:15 -0600

    fix: fixed issue about genre not showing on url on application startup if exists in local storage

--- src/context/genre/genre-provider.tsx
+++ src/context/genre/genre-provider.tsx
@@ -1,12 +1,25 @@
 'use client';
 
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { GenreContext } from './genre-context';
+import { useRouter } from 'next/navigation';
 
 export const GenreProvider = ({ children }: { children: ReactNode }) => {
   const [selectedGenre, setSelectedGenre] = useState<string | null>(
     typeof window !== 'undefined' ? localStorage.getItem('selectedGenre') : null
   );
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const genre = localStorage.getItem('selectedGenre');
+    if (genre) {
+      const queryParams = new URLSearchParams();
+      queryParams.set('genre', genre);
+      router.push(`catalog/?${queryParams}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const setGenre = (genre: string | null) => {
     setSelectedGenre(genre);
